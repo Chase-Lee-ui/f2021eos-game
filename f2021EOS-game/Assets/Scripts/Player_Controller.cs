c9879fd103dc693c937fbcfc2d953aa1ffa21cb2
Chase Lee <72054052+Chase-Lee-ui@users.noreply.github.com>
2021-12-08 01:07:28 -1000

    3D Movement

--- f2021EOS-game/Assets/Scripts/Player_Controller.cs
+++ f2021EOS-game/Assets/Scripts/Player_Controller.cs
@@ -4,59 +4,39 @@ using UnityEngine;
 
 public class Player_Controller : MonoBehaviour
 {
-    public float MoveSpeed;
-    public float TurnSpeed;
-    public float JumpHeight = 10f;
-    protected Rigidbody PlayerBody;
-    protected bool OnGround;
-    private float MaxSpeed = 20.0f;
-    void Start ()
-    {
-        this.PlayerBody = this.gameObject.GetComponent<Rigidbody>();
-    }
+    public CharacterController Controller;
+    public float Speed = 12.0f;
+    public float Gravity = -9.81f;
+    public float JumpHeight = 3.0f;
+    public Transform GroundCheck;
+    public float GroundDistance = 0.4f;
+    public LayerMask GroundMask;
+    Vector3 Velocity;
+    public bool IsGrounded;
     // Update is called once per frame
-    public virtual void Update ()
+    void Update()
     {
-        if(this.OnGround && Input.GetKeyDown(KeyCode.Space))
-        {
-            this.PlayerBody.AddForce(Vector3.up * this.JumpHeight);
-            this.OnGround = false;
-        }
-        //Moves Forward and back along z axis                           //Up/Down
-        var vertInput = Input.GetAxis("Vertical");
-            //Moves Forward and back along z axis                           //Up/Down
-            this.PlayerBody.AddRelativeForce(Vector3.forward * vertInput * this.MoveSpeed, ForceMode.Impulse);
-            if(!this.OnGround)
-            {
-                this.PlayerBody.AddRelativeForce(Vector3.down * 9.8f);
-            }
-            
-            if(this.PlayerBody.velocity.magnitude > this.MaxSpeed)
-            {
-                this.PlayerBody.velocity = this.PlayerBody.velocity.normalized * this.MaxSpeed;
-            }
+        this.IsGrounded = Physics.CheckSphere(this.GroundCheck.position, this.GroundDistance, this.GroundMask);
 
-            if(vertInput == 0)
-            {
-                this.PlayerBody.velocity = new Vector3(this.PlayerBody.velocity.x / 2.0f, this.PlayerBody.velocity.y, this.PlayerBody.velocity.z / 2.0f);
-            }
-        //Moves Left and right along x Axis                               //Left/Right
-        transform.Rotate(Vector3.up, this.TurnSpeed*Input.GetAxis("Horizontal")*Time.deltaTime);      
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if(collision.gameObject.CompareTag("Ground"))
+        if(this.IsGrounded && this.Velocity.y < 0)
         {
-            this.OnGround = true;
+            this.Velocity.y = -2f;
         }
-    }
 
-    private void OnCollisionExit(Collision collision)
-    {
-        if(collision.gameObject.CompareTag("Ground"))
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        this.Controller.Move(move * this.Speed * Time.deltaTime);
+
+        if(Input.GetKeyDown("space") && this.IsGrounded)
         {
-            this.OnGround = false;
+            this.Velocity.y = Mathf.Sqrt(this.JumpHeight * -2.0f * this.Gravity);
         }
+
+        this.Velocity.y += this.Gravity * Time.deltaTime;
+
+        this.Controller.Move(this.Velocity * Time.deltaTime);
     }
 }
