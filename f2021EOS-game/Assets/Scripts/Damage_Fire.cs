0785a6df2c2e46cd466fc147b4eff73bdbc83bca
Chase Lee <72054052+Chase-Lee-ui@users.noreply.github.com>
2021-12-30 18:50:32 -1000

    push

--- f2021EOS-game/Assets/Scripts/Damage_Fire.cs
+++ f2021EOS-game/Assets/Scripts/Damage_Fire.cs
@@ -6,42 +6,15 @@ public class Damage_Fire : MonoBehaviour
 {
     public float startTime;
     public float time;
-    public Health playerHealth;
     public float decay;
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerStay(Collider collision)
     {
-        //Time is a Unity engine object to detect time. deltaTime is the in-game time. Time.deltaTime is amount of time in between each frame update.
-        //question: why subtract the delta time from time?
         time -= Time.deltaTime;
         if(time <= 0)
         {
-            this.playerHealth.LifePoints -= decay;
+            collision.gameObject.GetComponent<Health>().LifePoints -= decay;
             time = startTime;
-        }
-    }
-    //if an object is in it, OnTriggerStay continues to detect the object as long as it's on the object.
-    //OnTriggerEnter only activates on first frame that it collides with object.
-    void OnTriggerEnter(Collider collision)
-    {
-
-        if (collision.gameObject.tag == "Player")
-        {
-            //LifePoints is the public float variable from the Health script.
-            this.playerHealth = collision.gameObject.GetComponent<Health>();
-        }
-    }
-
-
-    void OnTriggerExit(Collider collision)
-    {
-        //question: is the Collider collision referring to something attached to the player or the fire itself?
-        if (collision.gameObject.tag == "Player")
-        {
-            //LifePoints is the public float variable from the Health script.
-            //question: why null?
-            this.playerHealth = null;
         }
     }
 }
