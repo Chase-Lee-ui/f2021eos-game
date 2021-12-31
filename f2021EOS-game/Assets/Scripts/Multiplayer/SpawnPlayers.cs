f178c1c53234544b0cd7d6d6c92a667861bc9d81
Chase Lee <72054052+Chase-Lee-ui@users.noreply.github.com>
2021-12-30 14:53:11 -1000

    Organizing things around

--- f2021EOS-game/Assets/Scripts/Multiplayer/SpawnPlayers.cs
+++ f2021EOS-game/Assets/Scripts/Multiplayer/SpawnPlayers.cs
@@ -6,10 +6,15 @@ using Photon.Pun;
 public class SpawnPlayers : MonoBehaviour
 {
     public GameObject playerPrefab;
-
+    public Vector2 XRange;
+    public Vector2 YRange;
+    public Vector2 ZRange;
     private void Start()
     {
-        Vector3 spawnPosition = new Vector3(1, 1, 1);
+        Vector3 spawnPosition = new Vector3(
+            Random.Range(XRange.x, XRange.y), 
+            Random.Range(YRange.x, YRange.y), 
+            Random.Range(ZRange.x, ZRange.y));
         PhotonNetwork.Instantiate(playerPrefab.name, spawnPosition, Quaternion.identity);
     }
 }
